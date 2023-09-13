e67a80728f7d38570441dba83fd64dd8126f2a80
Igor Shaymukhametov <shaym@yandex.ru>
2023-09-13 15:09:25 +0500

    feat: add equality of objects exercise

---
+++ exercise_02_03.ts
@@ -0,0 +1,16 @@
+function deepEqual<T>(obj1: T, obj2: T): boolean {
+  if (typeof obj1 !== typeof obj2) return false;
+
+  if (typeof obj1 === 'object' && typeof obj2 === 'object' && obj1 !== null && obj2 !== null) {
+    const [keys1, keys2] = [obj1, obj2].map(Object.keys);
+    if (keys1.length !== keys2.length) return false;
+    for (let key of keys1) {
+      if (!deepEqual(obj1[key as keyof T], obj2[key as keyof T])) return false;
+    }
+    return true;
+  }
+
+  return obj1 === obj2;
+}
+
+export default deepEqual;
